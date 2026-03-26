3aa3fd7ca3f745b4364680d1dafe6f23a05ea132
Alekkzsx <alex.gss@puccampinas.edu.br>
2026-03-26 09:25:48 -0300

    u

--- src/ace/json/mod.rs
+++ src/ace/json/mod.rs
@@ -1,5 +1,5 @@
 pub use ace_json::{parse, stringify, JsonValue, JsonError};
-pub mod parser { pub use ace_json::parser::*; }
-pub mod serializer { pub use ace_json::serializer::*; }
-pub mod tokenizer { pub use ace_json::tokenizer::*; }
-pub mod value { pub use ace_json::value::*; }
+pub mod parser;
+pub mod serializer;
+pub mod tokenizer;
+pub mod value;
