96f390c69053430499b9254a632d028202dbac72
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-06-24 11:26:11 -0300

    refactor(main): split main() into smaller helper functions
    Extract 4 helper functions from main():
    - setup_tracing(): initialize tracing subscriber
    - parse_start_url(): parse CLI args for start URL
    - create_initial_tab(): create first tab
    - setup_timers(): setup system monitor and pulse timers
    - register_callbacks(): register all UI callbacks
    
    This reduces main() from 161 lines to under 30 lines.

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,49 @@ use albedo::ui;
 use albedo::browser::tabs::manager::TabManager;
 use slint::ComponentHandle;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize tracing subscriber for structured logging
+fn setup_tracing() {
     tracing_subscriber::fmt()
         .with_target(false)
         .with_thread_ids(false)
         .init();
+}
 
-    browser::setup::set_panic_hook();
+fn parse_start_url() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        if args[1].starts_with("http")
+            || args[1].starts_with("albedo:")
+            || args[1].starts_with("file:")
+        {
+            args[1].clone()
+        } else {
+            let path = std::env::current_dir().unwrap().join(&args[1]);
+            format!("file://{}", path.display())
+        }
+    } else {
+        "albedo://start".to_string()
+    }
+}
 
-    // Initialize Tokio Runtime
-    let rt = tokio::runtime::Runtime::new()?;
-    let _guard = rt.enter();
+fn create_initial_tab(
+    ui: &slint::Weak<ui::AppWindow>,
+    tab_manager: &TabManager,
+    tabs_model: &slint::VecModel<ui::TabData>,
+) {
+    let start_url = parse_start_url();
+    if let Some(ui) = ui.upgrade() {
+        tab_manager.create_tab(ui.window(), &start_url);
+        browser::bridge::sync_ace_visuals(&ui, tab_manager);
+        browser::events::sync_tabs(tab_manager, tabs_model);
+        ui.set_current_url(start_url.into());
+        ui.set_show_start_page(true);
+    }
+}
 
-    tracing::info!("Starting Albedo Browser");
-    let ui = browser::setup::create_window()?;
-    albedo_jit::contracts::core::register_json_parser(albedo::ace::json::parse);
-    let ui_handle = ui.as_weak();
-
-    let tab_manager = TabManager::new();
-
+fn setup_timers(
+    ui_handle: &slint::Weak<ui::AppWindow>,
+    tab_manager: &TabManager,
+) {
     // System Monitor
     let system = std::rc::Rc::new(std::cell::RefCell::new(AceSysInfo::new()));
     let sys_clone = system.clone();
@@ -39,71 +62,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
     );
 
-    // Tab Sync Logic (Initialization only now)
-    let tabs_model = std::rc::Rc::new(slint::VecModel::default());
-    ui.set_tabs_model(tabs_model.clone().into());
-
-    // Initial Tab Creation
-    let ui_handle_clone = ui_handle.clone();
-    let tm_clone = tab_manager.clone();
-    let tabs_model_clone = tabs_model.clone();
-
-    // Create first tab immediately
-    let args: Vec<String> = std::env::args().collect();
-    let start_url = if args.len() > 1 {
-        // If it's a local file path, format it as file:// if not already
-        if args[1].starts_with("http")
-            || args[1].starts_with("albedo:")
-            || args[1].starts_with("file:")
-        {
-            args[1].clone()
-        } else {
-            let path = std::env::current_dir().unwrap().join(&args[1]);
-            format!("file://{}", path.display())
-        }
-    } else {
-        "albedo://start".to_string()
-    };
-
-    if let Some(ui) = ui_handle_clone.upgrade() {
-        tm_clone.create_tab(ui.window(), &start_url);
-        browser::bridge::sync_ace_visuals(&ui, &tm_clone);
-        browser::events::sync_tabs(&tm_clone, &tabs_model_clone);
-        ui.set_current_url(start_url.into());
-        ui.set_show_start_page(true);
-    }
-
-    // Callbacks
-    let tm_clone = tab_manager.clone();
-    let ui_handle_clone = ui_handle.clone();
-    let tabs_model_clone = tabs_model.clone();
-
-    ui.on_navigate(move |url| {
-        browser::events::handle_navigate(&ui_handle_clone, &tm_clone, url, &tabs_model_clone);
-    });
-
-    let tm_clone = tab_manager.clone();
-    let tabs_model_clone = tabs_model.clone();
-    let ui_handle_clone = ui_handle.clone();
-
-    ui.on_request_new_tab(move || {
-        browser::events::handle_new_tab(&ui_handle_clone, &tm_clone, &tabs_model_clone);
-    });
-
-    let tm_clone = tab_manager.clone();
-    let tabs_model_clone = tabs_model.clone();
-    let ui_handle_clone = ui_handle.clone();
-
-    ui.on_request_switch_tab(move |index| {
-        browser::events::handle_switch_tab(&ui_handle_clone, &tm_clone, index, &tabs_model_clone);
-    });
-
-    let tm_clone = tab_manager.clone();
-    let tabs_model_clone = tabs_model.clone();
-    ui.on_request_close_tab(move |index| {
-        browser::events::handle_close_tab(&tm_clone, index, &tabs_model_clone);
-    });
-
     // JS Pulse Timer
     let tm_pulse = tab_manager.clone();
     let ui_pulse = ui_handle.clone();
@@ -115,6 +73,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             browser::events::handle_pulse(&ui_pulse, &tm_pulse);
         },
     );
+}
+
+fn register_callbacks(
+    ui: &ui::AppWindow,
+    tab_manager: &TabManager,
+    ui_handle: &slint::Weak<ui::AppWindow>,
+    tabs_model: &slint::VecModel<ui::TabData>,
+) {
+    let tm_clone = tab_manager.clone();
+    let ui_handle_clone = ui_handle.clone();
+    let tabs_model_clone = tabs_model.clone();
+    ui.on_navigate(move |url| {
+        browser::events::handle_navigate(&ui_handle_clone, &tm_clone, url, &tabs_model_clone);
+    });
+
+    let tm_clone = tab_manager.clone();
+    let tabs_model_clone = tabs_model.clone();
+    let ui_handle_clone = ui_handle.clone();
+    ui.on_request_new_tab(move || {
+        browser::events::handle_new_tab(&ui_handle_clone, &tm_clone, &tabs_model_clone);
+    });
+
+    let tm_clone = tab_manager.clone();
+    let tabs_model_clone = tabs_model.clone();
+    let ui_handle_clone = ui_handle.clone();
+    ui.on_request_switch_tab(move |index| {
+        browser::events::handle_switch_tab(&ui_handle_clone, &tm_clone, index, &tabs_model_clone);
+    });
+
+    let tm_clone = tab_manager.clone();
+    let tabs_model_clone = tabs_model.clone();
+    ui.on_request_close_tab(move |index| {
+        browser::events::handle_close_tab(&tm_clone, index, &tabs_model_clone);
+    });
 
     let tm_click = tab_manager.clone();
     let ui_click = ui_handle.clone();
@@ -155,6 +147,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ui.on_scroll(move |x, y, delta| {
         browser::events::handle_scroll(&ui_scroll, &tm_scroll, x, y, delta);
     });
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    setup_tracing();
+    browser::setup::set_panic_hook();
+
+    let rt = tokio::runtime::Runtime::new()?;
+    let _guard = rt.enter();
+
+    tracing::info!("Starting Albedo Browser");
+    let ui = browser::setup::create_window()?;
+    albedo_jit::contracts::core::register_json_parser(albedo::ace::json::parse);
+    let ui_handle = ui.as_weak();
+
+    let tab_manager = TabManager::new();
+    let tabs_model = std::rc::Rc::new(slint::VecModel::default());
+    ui.set_tabs_model(tabs_model.clone().into());
+
+    setup_timers(&ui_handle, &tab_manager);
+    create_initial_tab(&ui_handle, &tab_manager, &tabs_model);
+    register_callbacks(&ui, &tab_manager, &ui_handle, &tabs_model);
 
     ui.run()?;
     Ok(())
