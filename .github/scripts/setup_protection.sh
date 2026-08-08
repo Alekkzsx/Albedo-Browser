795fc75909f3d7b8628f5df452750036176b4bf8
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-08-08 18:05:47 -0300

    chore: remove required pull request review settings from branch protection script

--- .github/scripts/setup_protection.sh
+++ .github/scripts/setup_protection.sh
@@ -28,11 +28,7 @@ cat << 'EOF' > payload.json
     ]
   },
   "enforce_admins": true,
-  "required_pull_request_reviews": {
-    "dismiss_stale_reviews": true,
-    "require_code_owner_reviews": true,
-    "required_approving_review_count": 1
-  },
+  "required_pull_request_reviews": null,
   "restrictions": null,
   "required_linear_history": false,
   "allow_force_pushes": false,
