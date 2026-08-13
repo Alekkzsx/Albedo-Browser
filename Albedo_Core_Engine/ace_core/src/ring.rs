b7fa4732c3928a2df66760251918a9f3ed890615
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-08-13 08:44:45 -0300

    feat: implement arena allocator, string interner, and wait-free ring buffer primitives for ace_core

--- Albedo_Core_Engine/ace_core/src/ring.rs
+++ Albedo_Core_Engine/ace_core/src/ring.rs
@@ -113,27 +113,4 @@ impl<T> Drop for RingBuffer<T> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn test_spsc_ring_buffer() {
-        let ring = RingBuffer::new(3);
-        assert!(ring.push(10).is_ok());
-        assert!(ring.push(20).is_ok());
-        assert!(ring.push(30).is_ok());
-        
-        // A capacidade máxima é 3, o quarto vai falhar
-        assert!(ring.push(40).is_err());
-        
-        assert_eq!(ring.pop(), Some(10));
-        assert_eq!(ring.pop(), Some(20));
-        
-        assert!(ring.push(40).is_ok());
-        
-        assert_eq!(ring.pop(), Some(30));
-        assert_eq!(ring.pop(), Some(40));
-        assert_eq!(ring.pop(), None);
-    }
-}
