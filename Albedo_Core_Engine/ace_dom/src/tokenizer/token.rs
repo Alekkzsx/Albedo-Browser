66dc20242710031472442bb334cdce1a1b4f4b9d
Alekkzsx <alex.soares.gabriel111@gmail.com>
2026-08-26 17:06:33 -0300

    feat: define Token and CompactHTMLToken structures for HTML5 parsing and serialization

--- Albedo_Core_Engine/ace_dom/src/tokenizer/token.rs
+++ Albedo_Core_Engine/ace_dom/src/tokenizer/token.rs
@@ -59,3 +59,48 @@ pub enum Token {
     Null,
     Eof,
 }
+
+/// Token compacto e serializável otimizado para streaming assíncrono entre threads (Blink CompactHTMLToken pattern).
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CompactHTMLToken {
+    Doctype {
+        name: Option<SmolStr>,
+        public_id: Option<SmolStr>,
+        system_id: Option<SmolStr>,
+        force_quirks: bool,
+    },
+    StartTag {
+        name: Atom,
+        self_closing: bool,
+        attributes: InlineVec<Attribute, 4>,
+    },
+    EndTag {
+        name: Atom,
+    },
+    Comment(SmolStr),
+    Character(SmolStr),
+    Eof,
+}
+
+impl From<Token> for Option<CompactHTMLToken> {
+    fn from(tok: Token) -> Self {
+        match tok {
+            Token::Doctype(d) => Some(CompactHTMLToken::Doctype {
+                name: d.name,
+                public_id: d.public_identifier,
+                system_id: d.system_identifier,
+                force_quirks: d.force_quirks,
+            }),
+            Token::StartTag(s) => Some(CompactHTMLToken::StartTag {
+                name: s.name,
+                self_closing: s.self_closing,
+                attributes: s.attributes,
+            }),
+            Token::EndTag(e) => Some(CompactHTMLToken::EndTag { name: e.name }),
+            Token::Comment(c) => Some(CompactHTMLToken::Comment(c)),
+            Token::Character(c) => Some(CompactHTMLToken::Character(c)),
+            Token::Eof => Some(CompactHTMLToken::Eof),
+            Token::Null => None,
+        }
+    }
+}
